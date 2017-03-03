68c3639900b4436a49b67bf0523b9090ceaedf9c
Andre Prasetya Rahman <zery.dery@gmail.com>
2017-03-03 10:46:52 +0800

    Postest 1

---
+++ T.Postest1.1/src/Modulpertama/Pertama.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modulpertama;
+
+/**
+ *
+ * @author Spitzery
+ */
+public class Pertama {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       java.awt.EventQueue.invokeLater
+        (
+                new Runnable()
+                {
+                    public void run()
+                    {
+                        new frmHelloWord().setVisible(true);
+                    }
+                }
+        );
+    }
+    
+}
